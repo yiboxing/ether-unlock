507c66683e65f3ad1c10eb22626dc07951c5895f
Sim <me@xingyibo.com>
2018-11-10 22:46:52 -0800

    install request

--- api.js
+++ api.js
@@ -50,11 +50,11 @@ exports.PostTwitchRedeemCode = function(key, callback) {
       'Client-Id': config.twitch.client_id,
       'Authorization': config.twitch.auth_token,
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
-      ''
     },
     body: queryContent
   }
   request.post(options, callback)
+}
 //------------------------------------------------------------------------------
 //  Utils
 //------------------------------------------------------------------------------
